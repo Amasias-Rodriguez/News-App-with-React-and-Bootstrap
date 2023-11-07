504d22a5542caa9412eb3917fd0f55dd0263cc16
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-11-07 01:00:42 -0600

    Update Navbar.jsx

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 
-const Navbar = () => {
+const Navbar = ({ setCategory }) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
@@ -7,33 +7,27 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <div className="nav-link" onclick={() => setCategory("technology")}>Technology</div>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <div className="nav-link" onclick={() => setCategory("business")}>Business</div>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <div className="nav-link" onclick={() => setCategory("health")}>Health</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onclick={() => setCategory("science")}>Science</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onclick={() => setCategory("sports")}>Sports</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onclick={() => setCategory("entertaiment")}>Entertaiment</div>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
